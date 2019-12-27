a123e5a3a5ec7c1ae28fbd59cdd05b82be78a40e
GuillermoCalderaro <guillermocalderaro@gmail.com>
2019-12-26 21:12:18 -0300

    CourseContents: step6

---
+++ part2/courseinfo/src/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const Part = ({name, exercises}) => {
+
+    return (<p>{name} {exercises}</p>)
+}
+
+const Content = ({parts}) => {
+
+    const cont = () => 
+        parts.map(part => {
+            
+            return(
+            <Part key={part.id}
+                name={part.name}
+                exercises={part.exercises} />)   
+            }
+        )
+
+    return  (     
+        <>
+                {cont()}
+        </>
+    )
+}
+
+
+const Header = ({name}) =>  {
+    return (<h1>{name}</h1>)
+}
+
+const Course = ({course}) => (
+    <>
+        <Header name={course.name} />
+        <Content parts={course.parts}/>
+    </>
+)
+
+const App = () => {
+    const course = {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        }
+      ]
+    }
+  
+    return (
+      <div>
+        <Course course={course} />
+      </div>
+    )
+  }
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
